0c5b2bbd69d55a234d2f72f65a6a73cb030ecdc7
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-07-10 16:32:25 +0200

    aggiunto di bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'started';
+  mylat: number = 45;
+  mylng: number = 10;
+  updatemap() {
+    this.mylat = 110;
+    this.mylng = 50;
+  }
 }
--- src/app/components/gmap.ts
+++ src/app/components/gmap.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 // per far diventare un file un component di angular bisogna decorarlo
 @Component({
   // nome dell component
   selector: 'gmap',
-  template: `=>
-    <img
-      src="https://maps.googleapis.com/maps/api/staticmap?center=42,14&zoom=4&size=400x100&key="
-    />`,
-  inputs: ['lat', 'lng'],
+  template: ` <img
+    src="https://maps.googleapis.com/maps/api/staticmap?center={{ lat }},{{
+      lng
+    }}&zoom=4&size=400x100&key="
+  />`,
+  // inputs: [lat, lng],
 })
-export class MapComponent {}
+export class MapComponent {
+  // decoratore
+  @Input() lat: number | undefined;
+  @Input() lng: number | undefined;
+}
